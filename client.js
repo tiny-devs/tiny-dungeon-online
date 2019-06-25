48d5969bffde2e9a0646ed001e5ccaa658e390bc
andrendarcie <andrendarcie@gmail.com>
2019-06-25 15:35:36 -0300

    Update client.js

--- client.js
+++ client.js
@@ -1,20 +1,32 @@
 const readline = require('readline');
-var socket = require('socket.io-client')('http://localhost:8084');
 
-socket.on('connect', function () {
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+rl.question('> Type URL to connect: ', (url) => {
+    init(url);
+});
+
+function init(url) {
+    var socket = require('socket.io-client')(url);
+
+    socket.on('connect', function () {
+
+        socket.on('message', function (msg) {
+            console.log(msg);
+        });
+
+        loop ();
+
+        function loop () {
+            rl.question('> ', (message) => {
+                socket.send(message);
+                loop();
+            });
+        }
     });
-
-    socket.on('message', function (msg) {
-        console.log(msg);
-    });
-
-    rl.question('> ', (nome) => {
-        socket.send('Jogador: ' + nome + ' entrou na dungeon.');
-        rl.close();
-    });
-});  
+}
+  
 
