ddb64aa22a20322ece198b6bf946ebf279e1a59f
devsoitic03 <desenvolvimento03@soitic.com>
2020-05-08 13:34:44 -0300

    Add new files

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a player connected');
+
+    socket.on('walk-command', (msg) => {
+        console.log('walk-command: ' + msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
