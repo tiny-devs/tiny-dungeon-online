f4397376e4d4258a5b2f9fc4dce41e228dc9302b
André N. Darcie <andrendarcie@gmail.com>
2020-05-12 22:14:37 -0300

    Create player class (#1)

---
+++ player.js
@@ -0,0 +1,11 @@
+class Player {
+    constructor(id, name, color, x, y) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+    }
+}
+
+module.exports = Player;
