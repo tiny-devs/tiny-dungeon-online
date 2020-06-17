2776a0f39243bce62045688d2110b949906fe939
andrendarcie <andrendarcie@gmail.com>
2020-06-17 15:32:44 -0300

    Add tile system

---
+++ client/js/board/map/tiles/Color.js
@@ -0,0 +1,18 @@
+const Color = {
+    "Black": "#000000",
+    "DarkBlue": "#1D2B53",
+    "DarkPurple": "#7E2553",
+    "DarkGreen": "#008751",
+    "Brown": "#AB5236",
+    "DarkGrey": "#5F574F",
+    "LightGrey": "#C2C3C7",
+    "White": "#FFF1E8",
+    "Red": "#FF004D",
+    "Orange": "#FFA300",
+    "Yellow": "#FFEC27",
+    "Green": "#00E436",
+    "Blue": "#29ADFF",
+    "Lavender": "#83769C",
+    "Pink": "#FF77A8",
+    "LightPeach": "#FFCCAA",
+}
