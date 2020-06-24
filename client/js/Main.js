6cd06943ede25071b8213c8051731bd818bf9f49
Diego Penha <lumia-diguifi@hotmail.com>
2020-06-24 20:23:58 -0300

    Extremely basic mobile support (#54)

--- client/js/Main.js
+++ client/js/Main.js
@@ -1,13 +1,24 @@
 class Main {
     constructor(configs) {
       this.loginScreen = document.getElementById('login');
+      this.resetCanvasBtn = document.getElementById('help-draw-mobile');
+      this.mobileControls = document.getElementById('mobile-controls');
       this.playerNameInput = document.getElementById('player-name');
       this.confirmBtn = document.getElementById('confirm-btn');
+      this.mobileUp = document.getElementById('up');
+      this.mobileDown = document.getElementById('down');
+      this.mobileLeft = document.getElementById('left');
+      this.mobileRight = document.getElementById('right');
       this.gameScreen = document.getElementById('game');
       this.gameScreen.style.display='none';
       this.layersParentElement = document.getElementById('layers');
       this.layersParentElement.style.width = `${configs.game.width}px`;
       this.layersParentElement.style.height = `${configs.game.height}px`;
+
+      this.mobile = this.isMobile();
+      if (this.mobile) {
+        this.setupMobile();
+      }
       
       this.drawingGrid = new DrawingCanvas(this, configs.drawingGrid);
   
@@ -22,6 +33,33 @@ class Main {
       this.hasConfirmedName = false;
   
       this.confirmBtn.onclick = () => { this.onConfirmName() };
+      this.resetCanvasBtn.onclick = () => { this.drawingGrid.reset() };
+    }
+
+    isMobile() {
+      const toMatch = [
+          /Android/i,
+          /webOS/i,
+          /iPhone/i,
+          /iPad/i,
+          /iPod/i,
+          /BlackBerry/i,
+          /Windows Phone/i
+      ];
+  
+      return toMatch.some((toMatchItem) => {
+          return navigator.userAgent.match(toMatchItem);
+      });
+    }
+
+    setupMobile() {
+      document.body.addEventListener('touchmove', (e) => { 
+        e.preventDefault();
+      }, { passive: false });
+      const helpDesktop = document.getElementById('help-draw-desktop');
+      helpDesktop.style.display = 'none';
+      this.resetCanvasBtn.style.display = 'block';
+      this.layersParentElement.style.width = `${window.screen.width}px`;
     }
   
     onConfirmName() {
@@ -34,6 +72,10 @@ class Main {
               return arr.slice();
             });
              
+            if (this.mobile) {
+              const title = document.getElementById('title-content');
+              title.style.display = 'none';
+            }
             this.startGame();
           }
         }
