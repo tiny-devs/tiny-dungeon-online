ce8ddd9cdbbfa5a0e3fbbcf5a9386c47e9d075db
diguifi <lumia-diguifi@hotmail.com>
2021-01-17 20:01:35 -0300

    Add monsters to the map

--- server/entities/npcs/horse.ts
+++ server/entities/npcs/horse.ts
@@ -3,6 +3,6 @@ import NpcBase from './npcBase.ts'
 
 export default class Horse extends NpcBase {
     constructor() {
-        super(Npcs.Horse, false, 'horse', 0, 0, 0, 0, 1000, 0.25, 0, 36, null, [], null)
+        super(Npcs.Horse, false, 'horse', 0, 0, 0, 0, 1000, 0, 0, 36, null, [], null)
     }
 }
