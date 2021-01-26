f17acd83fe01b3b3e3e6c191dd481ebb5a942f72
diguifi <lumia-diguifi@hotmail.com>
2021-01-25 23:59:19 -0300

    Fix drop percentage of quest item

--- server/entities/npcs/cursedCactus.ts
+++ server/entities/npcs/cursedCactus.ts
@@ -5,6 +5,6 @@ import CactusJuice from '../items/cactusJuice.ts'
 
 export default class CursedCactus extends NpcBase {
     constructor() {
-        super(Npcs.CursedCactus, true, 'cursedcactus', 0, 0, 0, 40, 10000, 0.25, 6, 36, null, [new CactusJuice(1), new AdamantHelm(0.15)], null)
+        super(Npcs.CursedCactus, true, 'cursedcactus', 0, 0, 0, 40, 10000, 0.25, 6, 36, null, [new CactusJuice(0.1), new AdamantHelm(0.15)], null)
     }
 }
