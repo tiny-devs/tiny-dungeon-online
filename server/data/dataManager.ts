a0dd94a2465cb8c039a9c78dfd0e89147f17c54d
diguifi <lumia-diguifi@hotmail.com>
2022-07-12 23:12:33 -0300

    Removes dependency god_crypto

--- server/data/dataManager.ts
+++ server/data/dataManager.ts
@@ -1,31 +1,55 @@
-import { AES }  from "https://deno.land/x/god_crypto@v1.4.10/aes.ts";
-
 export default class DataManager {
-    private aes: AES
+    private aesKey: CryptoKey | undefined
+    private iv: Uint8Array
+    private aesAlgorithm = "AES-CBC"
 
     constructor() {
-        const aesKey = Deno.env.get('tinyaeskey') // the key must have a length of 16
+        const aesKey = Deno.env.get('tinyaeskey')
         const key = aesKey ? aesKey : 'defaultaeskey123'
-        this.aes = new AES(key, {
-            mode: "cbc",
-            iv: "tiny land online",
-        });
+        const encodedKey = new TextEncoder().encode(key)
+
+        this.iv = new TextEncoder().encode("tiny land online")
+        
+        crypto.subtle.importKey(
+            "raw",
+            encodedKey,
+            this.aesAlgorithm,
+            true,
+            ["encrypt", "decrypt"],
+          ).then((result: CryptoKey) => {
+                this.aesKey = result
+            }
+        )
     }
 
     public async encryptUserData(userData: string): Promise<string> {
-        const encryptedData = await this.aes.encrypt(userData)
-        return encryptedData.hex()
+        const encryptedData = await crypto.subtle.encrypt(
+            {name: this.aesAlgorithm, iv: this.iv},
+            this.aesKey!,
+            new TextEncoder().encode(userData),
+          )
+        return this.arrayBufferToHex(encryptedData)
     }
 
     public async decryptUserData(userData: string): Promise<string> {
         const bytes = this.hexToUint8Array(userData)
-        const decryptedData = await this.aes.decrypt(bytes)
-        return decryptedData.toString()
+        const decryptedData = await crypto.subtle.decrypt(
+            {name: this.aesAlgorithm, iv: this.iv},
+            this.aesKey!,
+            bytes,
+          )
+        return new TextDecoder("utf-8").decode(decryptedData)
     }
 
     private hexToUint8Array(data: string): Uint8Array {
-        let bytes = new Uint8Array(Math.ceil(data.length / 2));
+        const bytes = new Uint8Array(Math.ceil(data.length / 2));
         for (let i = 0; i < bytes.length; i++) bytes[i] = parseInt(data.substr(i * 2, 2), 16);
         return bytes
+    }
+
+    private arrayBufferToHex(buffer: ArrayBuffer) {
+        return [...new Uint8Array(buffer)]
+            .map(x => x.toString(16).padStart(2, '0'))
+            .join('')
     }
 }
