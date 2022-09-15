245676608bcee93b880538d7ff68321118f8fc9a
diguifi <lumia-diguifi@hotmail.com>
2022-09-15 20:27:23 -0300

    Update Deno

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as flags from "https://deno.land/std@0.148.0/flags/mod.ts"
-export { serve } from "https://deno.land/std@0.148.0/http/server.ts"
-export { serveFile } from "https://deno.land/std@0.148.0/http/file_server.ts"
+export * as flags from "https://deno.land/std@0.156.0/flags/mod.ts"
+export { serve } from "https://deno.land/std@0.156.0/http/server.ts"
+export { serveFile } from "https://deno.land/std@0.156.0/http/file_server.ts"
 export { Client } from "https://deno.land/x/mysql@v2.10.2/mod.ts";
