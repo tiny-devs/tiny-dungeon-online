5c5b1eed208d6d945dc8f19199fa5a6a291d2272
diguifi <lumia-diguifi@hotmail.com>
2024-01-22 21:40:41 -0300

    Upgrades Deno

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as flags from "https://deno.land/std@0.202.0/flags/mod.ts"
-export { serve } from "https://deno.land/std@0.202.0/http/server.ts"
-export { serveFile } from "https://deno.land/std@0.202.0/http/file_server.ts"
+export * as flags from "https://deno.land/std@0.212.0/flags/mod.ts"
+export { serve } from "https://deno.land/std@0.212.0/http/server.ts"
+export { serveFile } from "https://deno.land/std@0.212.0/http/file_server.ts"
 //export { Client } from "https://deno.land/x/mysql@v2.10.2/mod.ts";
