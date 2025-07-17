595ec6b037043523d82789811ee307a208bd6a08
diguifi <lumia-diguifi@hotmail.com>
2025-07-16 21:09:45 -0300

    Upgrade Deno

--- deps.ts
+++ deps.ts
@@ -1,3 +1,2 @@
-export { parseArgs } from "https://jsr.io/@std/cli/1.0.17/parse_args.ts"
-export { serveFile } from "https://jsr.io/@std/http/1.0.15/mod.ts"
-//export { Client } from "https://deno.land/x/mysql@v2.10.2/mod.ts";
+export { parseArgs } from "https://jsr.io/@std/cli/1.0.20/parse_args.ts"
+export { serveFile } from "https://jsr.io/@std/http/1.0.19/mod.ts"
