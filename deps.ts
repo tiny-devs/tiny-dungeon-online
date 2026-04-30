9cd47187fd93bf1d0d776d741e861e63a3289e93
diguifi <lumia-diguifi@hotmail.com>
2026-04-30 10:52:21 -0300

    Upgrade Deno

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { parseArgs } from "https://jsr.io/@std/cli/1.0.21/parse_args.ts"
-export { serveFile } from "https://jsr.io/@std/http/1.0.20/mod.ts"
+export { parseArgs } from "https://jsr.io/@std/cli/1.0.29/parse_args.ts"
+export { serveFile } from "https://jsr.io/@std/http/1.1.0/mod.ts"
