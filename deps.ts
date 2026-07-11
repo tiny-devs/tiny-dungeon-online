19fe71732ed8a87622482b98e49c1b50d03b5542
diguifi <lumia-diguifi@hotmail.com>
2026-07-10 21:37:28 -0300

    Update deps

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { parseArgs } from "https://jsr.io/@std/cli/1.0.29/parse_args.ts"
-export { serveFile } from "https://jsr.io/@std/http/1.1.0/mod.ts"
+export { parseArgs } from "https://jsr.io/@std/cli/1.0.32/parse_args.ts"
+export { serveFile } from "https://jsr.io/@std/http/1.1.2/mod.ts"
